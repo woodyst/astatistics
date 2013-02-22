df8c0d4459305e61224ed85d6cb8c2a990af8720
Eduardo García-Mádico Portabella <edi@eurogaran.com>
2013-02-22 01:40:19 +0100

    modified:   db/asterisk-mysql.sql new file:   db/trigger_bi_queueEvents.sql
    Added queue information in agent_status table via
    asterisk.bi_queueEvents trigger in MySQL DB so queue information is
    showed correctly in that table.

--- db/asterisk-mysql.sql
+++ db/asterisk-mysql.sql
@@ -153,17 +153,17 @@ DELIMITER //
 CREATE TRIGGER `asterisk`.`bi_queueEvents` BEFORE INSERT ON `asterisk`.`queue_log`
 FOR EACH ROW BEGIN
 IF NEW.event = 'ADDMEMBER' THEN
-INSERT INTO agent_status (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO agent_status (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'REMOVEMEMBER' THEN
-INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'LOGGEDOUT',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDOUT", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'LOGGEDOUT',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDOUT", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'AGENTLOGIN' THEN
-INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'LOGGEDIN',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDIN", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'LOGGEDIN',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDIN", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'AGENTLOGOFF' THEN
-INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'LOGGEDOUT',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDOUT", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'LOGGEDOUT',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "LOGGEDOUT", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'PAUSE' THEN
-INSERT INTO agent_status (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'PAUSE',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "PAUSE", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO agent_status (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'PAUSE',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "PAUSE", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'UNPAUSE' THEN
-INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO `agent_status` (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event = 'ENTERQUEUE' THEN
 REPLACE INTO `call_status` VALUES
 (NEW.callid,
@@ -185,14 +185,16 @@ timestamp = FROM_UNIXTIME(NEW.time),
 queue = NEW.queuename,
 holdtime = replace(substring(substring_index(NEW.data, '|', 1), length(substring_index(NEW.data, '|', 1 - 1)) + 1), '|', '')
 where callid = NEW.callid;
-INSERT INTO agent_status (agentId,agentStatus,timestamp,callid) VALUES
+INSERT INTO agent_status (agentId,agentStatus,timestamp,callid,queue) VALUES
 (NEW.agent,NEW.event,
 FROM_UNIXTIME(NEW.time),
-NEW.callid)
+NEW.callid,
+NEW.queuename)
 ON DUPLICATE KEY UPDATE
 agentStatus = NEW.event,
 timestamp = FROM_UNIXTIME(NEW.time),
-callid = NEW.callid;
+callid = NEW.callid,
+queue = NEW.queuename;
 ELSEIF NEW.event in ('COMPLETECALLER','COMPLETEAGENT') THEN
 UPDATE `call_status` SET
 callid = NEW.callid,
@@ -203,7 +205,7 @@ originalPosition = replace(substring(substring_index(NEW.data, '|', 3), length(s
 holdtime = replace(substring(substring_index(NEW.data, '|', 1), length(substring_index(NEW.data, '|', 1 - 1)) + 1), '|', ''),
 callduration = replace(substring(substring_index(NEW.data, '|', 2), length(substring_index(NEW.data, '|', 2 - 1)) + 1), '|', '')
 where callid = NEW.callid;
-INSERT INTO agent_status (agentId,agentStatus,timestamp,callid) VALUES (NEW.agent,NEW.event,FROM_UNIXTIME(NEW.time),NULL) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL;
+INSERT INTO agent_status (agentId,agentStatus,timestamp,callid,queue) VALUES (NEW.agent,NEW.event,FROM_UNIXTIME(NEW.time),NULL,NEW.queuename) ON DUPLICATE KEY UPDATE agentStatus = "READY", timestamp = FROM_UNIXTIME(NEW.time), callid = NULL, queue = NEW.queuename;
 ELSEIF NEW.event in ('TRANSFER') THEN
 UPDATE `call_status` SET
 callid = NEW.callid,
@@ -213,12 +215,13 @@ queue = NEW.queuename,
 holdtime = replace(substring(substring_index(NEW.data, '|', 1), length(substring_index(NEW.data, '|', 1 - 1)) + 1), '|', ''),
 callduration = replace(substring(substring_index(NEW.data, '|', 3), length(substring_index(NEW.data, '|', 3 - 1)) + 1), '|', '')
 where callid = NEW.callid;
-INSERT INTO agent_status (agentId,agentStatus,timestamp,callid) VALUES
-(NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL)
+INSERT INTO agent_status (agentId,agentStatus,timestamp,callid,queue) VALUES
+(NEW.agent,'READY',FROM_UNIXTIME(NEW.time),NULL,NEW.queuename)
 ON DUPLICATE KEY UPDATE
 agentStatus = "READY",
 timestamp = FROM_UNIXTIME(NEW.time),
-callid = NULL;
+callid = NULL,
+queue = NEW.queuename;
 ELSEIF NEW.event in ('ABANDON','EXITEMPTY') THEN
 UPDATE `call_status` SET
 callid = NEW.callid,
