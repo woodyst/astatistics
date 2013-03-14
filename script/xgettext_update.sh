2944c4536328a56075bb5fa276faee0bc39d5b04
Eduardo García-Mádico Portabella <edi@eurogaran.com>
2013-03-14 22:36:23 +0100

    modified:   lib/Astatistics/I18N/es.po modified:   lib/Astatistics/I18N/messages.pot modified:   root/src/stats/show.tt modified:   root/src/stats/template_edit.tt modified:   root/src/stats/templates.tt modified:   script/xgettext_update.sh
    More translation code and Spanish translations.

--- script/xgettext_update.sh
+++ script/xgettext_update.sh
@@ -3,4 +3,5 @@
 xgettext.pl --output=lib/Astatistics/I18N/messages.pot --directory=lib/ --directory=root/
 for i in lib/Astatistics/I18N/*.po; do
 	msgmerge -U $i lib/Astatistics/I18N/messages.pot
+	vi $i
 done
